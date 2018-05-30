58c157ccdea4bf5eeeb329487f5f0bfeaefb3ea3
Cristian Cantero <cristianemelcanteroalvarez@gmail.com>
2018-05-30 10:37:02 -0400

    Directorio definitivo

--- main.js
+++ main.js
@@ -4,11 +4,8 @@ const url = require('url')
 //const mysql = require('mysql')
 // Adicion de librerias esenciales
 
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow, ipcMain} = electron
 // Creacion de objetos
-
-const interfaces = 'interfaces/'
-// Definicion de variables de directorios
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -16,6 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
+  // Hay que agregar botones de minimizar, maximizar y cerrar
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -23,49 +21,59 @@ function createWindow () {
     frame: false,
     resizable: true,
     backgroundColor: '#2e2c29',
-    fullscreen: true,
     title: 'Visual',
     opacity: 0.5
   })
 
-  mainWindow.once('ready-to-show', () =>{
-    mainWindow.show()
-  })
+const interfaces = 'interfaces/'
+// Definicion de variables de directorios
+ipcMain.on('fullscreen', () => {
+	if (mainWindow.isFullScreen())
+		mainWindow.setFullScreen(false)
+	else
+		mainWindow.setFullScreen(true)
+})
+ipcMain.on('minimize', () => { mainWindow.minimize() })
+ipcMain.on('maximize', () => {
+	if (mainWindow.isMaximized())
+		mainWindow.unmaximize()
+	else
+		mainWindow.maximize()
+})
+ipcMain.on('close', () => { mainWindow.close() })
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(
-    url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-    })
-  )
-
-
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', function (){
-
+mainWindow.once('ready-to-show', () =>{
   if(false){
       //Si es la primera vez, configura la BD y demas cosas
   }else{
     createWindow()
   }
 })
+
+  // and load the index.html of the app.
+mainWindow.loadURL(
+    url.format({
+    pathname: path.join(__dirname, 'View/index.html'),
+    protocol: 'file:',
+    slashes: true
+    })
+)
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+  mainWindow = null
+})
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
