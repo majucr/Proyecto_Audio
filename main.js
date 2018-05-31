ce29b6f716d2bf51dd6fec7fdd11faa79d45afef
Cristian Cantero <cristianemelcanteroalvarez@gmail.com>
2018-05-30 20:22:48 -0400

    tantas cosas

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-const controller = require('controller')
 const electron = require('electron')
 const path = require('path')
 const url = require('url')
-
 //const mysql = require('mysql')
 // Adicion de librerias esenciales
 
@@ -13,41 +11,58 @@ const {app, BrowserWindow, ipcMain} = electron
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow ()
-{
-  // Create the browser window.
-  // Hay que agregar botones de minimizar, maximizar y cerrar
-  mainWindow = new BrowserWindow(
-  {
+function createWindow () {
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     show: false,
     frame: false,
     resizable: true,
     backgroundColor: '#2e2c29',
-    title: 'Visual',
     opacity: 0.5
+  })
+
+  mainWindow.loadURL(
+    url.format({
+    pathname: path.join(__dirname, 'View/login.html'),
+    protocol: 'file:',
+    slashes: true
+    })
+  )
+
+  mainWindow.once('ready-to-show', () =>{
+    mainWindow.show()
+  })
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
   })
 }
 
 const interfaces = 'interfaces/'
 // Definicion de variables de directorios
 ipcMain.on('fullscreen', () => {
+
 	if (mainWindow.isFullScreen())
 		mainWindow.setFullScreen(false)
 	else
 		mainWindow.setFullScreen(true)
+
 })
+
 ipcMain.on('minimize', () => { mainWindow.minimize() })
 ipcMain.on('maximize', () => {
+
 	if (mainWindow.isMaximized())
 		mainWindow.unmaximize()
 	else
 		mainWindow.maximize()
+
 })
 ipcMain.on('close', () => { mainWindow.close() })
 
-mainWindow.once('ready-to-show', () =>{
+app.on('ready', function (){
+
   if(false){
       //Si es la primera vez, configura la BD y demas cosas
   }else{
@@ -55,35 +70,7 @@ mainWindow.once('ready-to-show', () =>{
   }
 })
 
-  // and load the index.html of the app.
-mainWindow.loadURL(
-    url.format({
-    pathname: path.join(__dirname, 'View/login.html'),
-    protocol: 'file:',
-    slashes: true
-    })
-)
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-  mainWindow = null
-})
-
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
@@ -91,8 +78,6 @@ app.on('window-all-closed', function () {
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
